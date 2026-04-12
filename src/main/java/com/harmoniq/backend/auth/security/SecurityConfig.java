2037c2989ded0e37688173e253dfbee6950ac346
rohith <rohith.officia@gmail.com>
2026-04-12 17:35:25 +0530

    fix backend

--- src/main/java/com/harmoniq/backend/auth/security/SecurityConfig.java
+++ src/main/java/com/harmoniq/backend/auth/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://your-frontend.vercel.app"
+                "https://harmoniqfrontend.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
